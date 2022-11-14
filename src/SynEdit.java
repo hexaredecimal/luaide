e30174df6471ffaa1b9a69c022b2a5f563076ec6
Sibusiso Vincent <51314855+vultureofficial@users.noreply.github.com>
2022-11-14 14:23:11 +0200

    initial commit

---
+++ src/SynEdit.java
@@ -0,0 +1,209 @@
+package luaide;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import org.fife.ui.rtextarea.*;
+import org.fife.ui.rsyntaxtextarea.*;
+import java.io.*;
+
+public class SynEdit extends JPanel implements ActionListener {
+
+    RSyntaxTextArea textArea = new RSyntaxTextArea(20, 60);
+    JFrame frame = new JFrame("Input Dialog Box Frame");
+    JButton button = new JButton("Show Input Dialog Box");
+    JFileChooser fc = new JFileChooser();
+    private JTextArea ta;
+    private int count;
+    private JMenuBar menuBar;
+    private JMenu fileM;
+    private JMenuItem exitI, saveI, compileI, aboutI;
+    private String pad;
+    private JToolBar toolBar;
+    protected File selectedFile; 
+    private RTextScrollPane sp; 
+
+    public boolean canUndo() {
+        return textArea.canUndo(); 
+    }
+
+    public boolean canRedo() {
+        return textArea.canRedo(); 
+    }
+
+    public boolean hasFile() {
+        return selectedFile != null; 
+    }
+    
+    public void selectAllText() {
+        textArea.selectAll();
+    }
+    
+    public void selectTextAt(int start, int end) {
+        textArea.select(start, end);
+    }
+    
+    public String getFileName() {
+        return hasFile() ? selectedFile.getName(): null;
+    }
+    
+    public void setFile(File file) {
+        selectedFile = file; 
+    }
+
+    public File getFile() {
+        return selectedFile;
+    }
+   
+    public void doUndo() {
+        textArea.undoLastAction(); 
+    }
+
+    public void doRedo() {
+        textArea.redoLastAction();
+    }
+    
+    
+    public String getText() {
+        return textArea.getText();
+    }
+    
+    public void setText(String txt) {
+        textArea.setText(txt);
+    }
+    
+    public SynEdit() {
+        this.setLayout(new BorderLayout());
+        //super("Text Editor");
+        //setSize(600, 600);
+        //setLocationRelativeTo(null);
+        //setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        //Container pane = getContentPane();
+        //pane.setLayout(new BorderLayout());
+
+        //JTree trr = new JTree();
+        textArea.setSyntaxEditingStyle(SyntaxConstants.SYNTAX_STYLE_LUA);
+        textArea.setCodeFoldingEnabled(true);
+        sp = new RTextScrollPane(textArea);
+        sp.setFoldIndicatorEnabled(true);
+        sp.setIconRowHeaderEnabled(true);
+        
+        this.add(sp);
+
+        count = 0;
+        pad = " ";
+        ta = new JTextArea(); //textarea
+
+        menuBar = new JMenuBar(); //menubar
+        fileM = new JMenu("File"); //file menu
+        
+        exitI = new JMenuItem("Exit");  //menu items on File Menu
+        saveI = new JMenuItem("Save");  //menu items on File Menu
+        compileI = new JMenuItem("Compile"); //menu items on File Menu
+        aboutI = new JMenuItem("About");
+        toolBar = new JToolBar();
+
+        ta.setLineWrap(true);
+        ta.setWrapStyleWord(true);
+
+        //setJMenuBar(menuBar);
+        menuBar.add(fileM); // "FILE" on the menu bar
+        fileM.add(aboutI); // "About" on the menu bar
+        fileM.add(saveI); // "Save" on the menu item for FILE
+        fileM.add(compileI); // "Complie" on the menu item for FILE
+        fileM.add(exitI); // "Exit" on the menu item for FILE
+
+        saveI.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S, ActionEvent.CTRL_MASK));
+
+        //pane.add(toolBar, BorderLayout.SOUTH);
+
+        saveI.addActionListener(this);
+        exitI.addActionListener(this);
+        compileI.addActionListener(this);
+        aboutI.addActionListener(this);
+
+        setVisible(true);
+
+    }
+
+    void writetofile(File ff) throws Exception {
+        FileWriter fw = new FileWriter(ff.getAbsoluteFile());
+        BufferedWriter bw = new BufferedWriter(fw);
+        bw.write(textArea.getText());
+        bw.close();
+    }
+
+    private static void printLines(String name, InputStream ins) throws Exception {
+        String line = null;
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(ins));
+        while ((line = in.readLine()) != null) {
+            System.out.println(name + " " + line);
+        }
+    }
+
+    private static void runProcess(String command) throws Exception {
+        Process pro = Runtime.getRuntime().exec(command);
+        printLines(command + "\n============================================================\nOUTPUT:\n\n\n\n", pro.getInputStream());
+        printLines(command + " stderr:", pro.getErrorStream());
+        pro.waitFor();
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        JMenuItem choice = (JMenuItem) e.getSource();
+
+        if (choice == saveI) {
+            int returnVal = fc.showSaveDialog(SynEdit.this);
+
+            if (returnVal == JFileChooser.APPROVE_OPTION) {
+                try {
+                    File file = fc.getSelectedFile();
+                    writetofile(file);
+                } catch (Exception esa) {
+
+                }
+
+            }
+        } else if (choice == exitI) {
+            System.exit(0);
+        } else if (choice == compileI) {
+            Boolean flg = true;
+
+            do {
+                String str = JOptionPane.showInputDialog(null, "Enter class name : ", "Text Editor", 1);
+                if (str != null) {
+                    if (str.equals("")) {
+                        JOptionPane.showMessageDialog(null, "ENTER VALID NAME", "Text Editor", 1);
+                    } else {
+                        try {
+                            flg = false;
+                            File file = new File(str + ".java");
+                            FileWriter fw = new FileWriter(file.getAbsoluteFile());
+                            BufferedWriter bw = new BufferedWriter(fw);
+                            bw.write(textArea.getText());
+                            bw.close();
+                            Process pro1 = Runtime.getRuntime().exec("javac " + str + ".java");
+                            pro1.waitFor();
+                            runProcess("java " + str);
+                        } catch (Exception es) {
+                            es.printStackTrace();
+                        }
+                    }
+                } else {
+                    flg = false;
+                    break;
+                }
+
+            } while (flg == true);
+        } else if (choice == aboutI) {
+            JOptionPane.showMessageDialog(null, "Text Editor in Java. \n\n 2014", "Text Editor", 1);
+        }
+
+    }
+//
+//    public static void main(String[] args) {
+//        new SynEdit();
+//    }
+
+}
